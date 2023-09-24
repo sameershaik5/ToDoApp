bdcb17acf37e1aa2080c736a32e77320524b0190
sameershaik5 <35886034+sameershaik5@users.noreply.github.com>
2023-09-24 18:05:10 +0530

    Add files via upload

---
+++ ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿namespace ToDoApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime DueDate { get; set; }
+
+        // Foreign key for the user who owns this TODO item
+        public string UserId { get; set; }
+
+        // Navigation property to represent the owner/user
+        public AppUser User { get; set; }
+    }
+
+}
